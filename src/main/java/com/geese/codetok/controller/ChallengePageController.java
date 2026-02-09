33d74ff33bba63bfb6ba38f1b26e0a4038f64588
Pierre caillous <Pierre.cailloupgmail.com>
2026-02-09 17:37:36 +0100

    add page controller and challengePage controller

---
+++ src/main/java/com/geese/codetok/controller/ChallengePageController.java
@@ -0,0 +1,41 @@
+package com.geese.codetok.controller;
+
+import ch.qos.logback.core.model.Model;
+import com.geese.codetok.model.CodeProblem;
+import com.geese.codetok.service.CodeProblemService;
+import com.geese.codetok.service.PromptTopics;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ChallengePageController {
+    private final CodeProblemService codeProblemService;
+    private final PromptTopics promptTopics;
+
+    public ChallengePageController(CodeProblemService codeProblemService, PromptTopics promptTopics) {
+        this.codeProblemService = codeProblemService;
+        this.promptTopics = promptTopics;
+    }
+
+    @GetMapping("/challenge-page")
+    public String showChallengePage(
+            @RequestParam(defaultValue = "beginner") String level,
+            Model model) {
+        String topic;
+        if (level.equalsIgnoreCase("intermediate")) {
+            topic = promptTopics.getRandomTopicIntermediate();
+        } else if (level.equalsIgnoreCase("expert")) {
+            topic = promptTopics.getRandomTopicExpert();
+        } else {
+            topic = promptTopics.getRandomTopicBeginner();
+        }
+        CodeProblem problem = codeProblemService.createNewProblem(level, topic);
+
+        // 3. Pass the object to the HTML
+        model.addText("problem");
+        model.addText("currentLevel");
+
+        return "challenge";
+    }
+}
--- src/main/java/com/geese/codetok/controller/PageController.java
+++ src/main/java/com/geese/codetok/controller/PageController.java
@@ -1,6 +1,18 @@
 package com.geese.codetok.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class PageController {
 
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/play")
+    public String challengePage() {
+        return "challenge";
+    }
 }
