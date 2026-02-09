6834cf1d872d7ea8516307dcf3a2399e6313b311
Pierre caillous <Pierre.cailloupgmail.com>
2026-02-09 13:37:01 +0100

    start repository layer

--- src/main/java/com/geese/codetok/repository/UserRepository.java
+++ src/main/java/com/geese/codetok/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package com.geese.codetok.repository;
 
-import org.apache.catalina.User;
+import com.geese.codetok.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
